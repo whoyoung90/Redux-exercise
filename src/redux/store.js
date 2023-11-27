9625e89cf84fc4cba6e58040f2481242c50a81db
kim-wooyoung <whoyoung90@naver.com>
2023-11-27 11:37:46 +0900

    study: redux-devtools 연동하기

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,9 @@ import { createStore } from "redux";
 import rootReducer from "./reducers";
 
 // 현재 redux-Toolkit을 사용하는것이 표준 (createStore는 deprecated..)
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
